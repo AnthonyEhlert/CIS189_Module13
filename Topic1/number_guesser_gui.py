8cc7c361f6c5b75595da7c65bbd466b784253778
Tony Ehlert <aehlert@dmacc.edu>
2023-04-12 07:11:35 -0500

    initial commit, basic GUI format/setup

---
+++ Topic1/number_guesser_gui.py
@@ -0,0 +1,35 @@
+"""
+Program: number_guesser.gui
+Author: Tony Ehlert
+Last date modified: 4/12/2023
+
+The purpose of this program is generate a GUI that creates a NumberGuesser object and allows the user to play a
+number guessing game
+
+The input is data needed to create and run the GUI along with user input via mouse clicks
+The output is GUI displays of the game results
+"""
+import tkinter
+
+'''
+FUNCTIONS BELOW HERE
+'''
+
+
+'''
+FUNCTIONS ABOVE HERE
+'''
+
+# create main window
+main_window = tkinter.Tk()
+
+'''
+MODULE CODE BELOW HERE
+'''
+
+'''
+MODULE CODE ABOVE HERE
+'''
+
+# run main_window.mainloop()
+main_window.mainloop()
